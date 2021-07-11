e10c62c2079f6d7c523460d96543ae6c59446693
NeoTRAN001 <neo_tran@outlook.com>
2021-07-10 22:30:35 -0500

    feat: load url

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ app.on('before-quit', () => { // Evento antes de salir
 });
 
 app.on('ready', () => { // Evento cuando la app este lista
-    let win = new BrowserWindow();
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Hola mundo',
+        resizable: false,
+        maximizable: false,
+        center: true,
+        show: false
+    });
+
+    win.once('ready-to-show', () => {
+        win.show();
+    })
+
+    win.on('move', () => { // Evento que se activa por cada movimiento de la ventana
+        const position = win.getPosition();
+        console.log(position);
+    });
 
     win.on('closed', () => { // Evento al cerrar la ventana
         win = null;
         app.quit();
     });
+
+    win.loadURL('http://devdocs.io/')
 });
